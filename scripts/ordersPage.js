6de41b620437ea9a18cffef5169424867a0a76ab
mohammadteimory <mohammadteimorystore@gmail.com>
2025-10-20 09:30:42 +0330

    feat: update cart quantity display and enhance order page event handling

--- scripts/ordersPage.js
+++ scripts/ordersPage.js
@@ -3,16 +3,22 @@ import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import { formattCurrency } from "./utils/money.js";
 import { findMatchingItem } from "./utils/findMatchingItem.js";
 import { loadProductsFech, products } from "../data/products.js";
+import { cart } from "../data/cart.js";
+import { updateCartQunUi } from "./utils/cartQuanUi.js";
 
 async function loadPage() {
   await loadProductsFech();
+  renderOrdersPage();
+  orderPageEvents();
+  updateCartQunUi();
+}
 
-  function renderOrdersPage() {
-    document.querySelector(".js-orders-grid").innerHTML = orders
-      .map((order) => {
-        const orderTimeString = dayjs(order.orderTime).format("MMMM D");
-        console.log(order);
-        return `
+const mainContainer = document.querySelector(".js-orders-grid");
+function renderOrdersPage() {
+  mainContainer.innerHTML = orders
+    .map((order) => {
+      const orderTimeString = dayjs(order.orderTime).format("MMMM D");
+      return `
         <div class="order-container">
           <div class="order-header">
             <div class="order-header-left-section">
@@ -37,20 +43,20 @@ async function loadPage() {
           </div>
         </div>
     `;
-      })
-      .join("");
-  }
+    })
+    .join("");
+}
 
-  function renderOrdersDetails(order) {
-    return order.products
-      .map((productDetails) => {
-        const matchingItem = findMatchingItem(
-          products,
-          productDetails.productId,
-          "id"
-        );
+function renderOrdersDetails(order) {
+  return order.products
+    .map((productDetails) => {
+      const matchingItem = findMatchingItem(
+        products,
+        productDetails.productId,
+        "id"
+      );
 
-        return `
+      return `
         <div class="product-image-container">
             <img
             src="${matchingItem.image}"
@@ -67,24 +73,46 @@ async function loadPage() {
             <div class="product-quantity">Quantity: ${
               productDetails.quantity
             }</div>
-            <button class="buy-again-button button-primary">
-            <img class="buy-again-icon" src="images/icons/buy-again.png" />
-            <span class="buy-again-message">Buy it again</span>
+            <button class="buy-again-button button-primary js-buy-again-button"
+                data-product-id="${matchingItem.id}">
+                <img class="buy-again-icon" src="images/icons/buy-again.png" />
+                <span class="buy-again-message ">Buy it again</span>
             </button>
         </div>
 
         <div class="product-actions">
-            <a href="tracking.html?orderId=${order.id}&productId=${matchingItem.id}">
-            <button class="track-package-button button-secondary">
-                Track package
-            </button>
+            <a 
+                href="tracking.html?orderId=${order.id}&productId=${
+                matchingItem.id
+                }">
+                <button class="track-package-button button-secondary">
+                    Track package
+                </button>
             </a>
         </div>
 `;
-      })
-      .join("");
-  }
-  renderOrdersPage();
+    })
+    .join("");
 }
+
+function orderPageEvents() {
+  mainContainer.addEventListener("click", (e) => {
+    const button = e.target.closest(".js-buy-again-button");
+    if (button) {
+      const productId = button.dataset.productId;
+      cart.addToCart(productId, 1);
+      updateCartQunUi();
+      button.innerHTML = "Added";
+      setTimeout(() => {
+        button.innerHTML = `
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        `;
+      }, 1000);
+    }
+  });
+}
+
 loadPage();
 
+//الان باید دکمه ی پیگیری سفارش پویا کنم
